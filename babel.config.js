c412b036c5a9dedcb78205e395537c5c0a9f1dae
pangwei <pw374644043@gmail.com>
2022-07-28 16:33:28 +0800

    完成项目基础构建

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: pangwei pw374644043@gmail.com
+ * @Date: 2022-07-26 13:22:14
+ * @LastEditors: pangwei pw374644043@gmail.com
+ * @LastEditTime: 2022-07-26 13:22:35
+ */
+module.exports = {
+  presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
+}
---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: pangwei pw374644043@gmail.com
+ * @Date: 2022-07-25 14:07:12
+ * @LastEditors: pangwei pw374644043@gmail.com
+ * @LastEditTime: 2022-07-25 16:19:36
+ */
+import validate from './lib/validate'
+
+export default {
+  validate,
+}
